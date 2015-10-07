3d7c91b0283b9c7ba71775588b16f54d8cfc7a82
witwave <witwave@outlook.com>
2015-10-08 06:34:22 +0800

    ...

---
+++ resources/views/include/footer.blade.php
@@ -0,0 +1,106 @@
+
+
+      <!-- Footer
+      =================================================== -->
+      <footer class="footer-block white-bg">
+
+        <!-- Container -->
+        <div class="container cont-top clearfix">
+
+          <!-- Row -->
+          <div class="row">
+
+            <!-- Brand -->
+            <div class="col-md-3 brand-col brand-center">
+              <div class="vcenter">
+                <a class="vcenter-this" href="#">
+
+                  <img src="images/logo1.png" alt="BELOVED">
+                </a>
+              </div>
+            </div>
+            <!-- /Brand -->
+
+            <!-- Links -->
+            <div class="col-md-9 links-col">
+
+              <!-- Row -->
+              <div class="row-fluid">
+
+                <!-- Col -->
+                <div class="col-xs-6 col-sm-6 col-md-6">
+                  <h5>关于我们</h5>
+                  <p>蓓爱花坊（Beloved）品牌，隶属于BD集团，以体验式超现实花品为引，让人体会真实幸福一刻，让蓓爱花坊相随一辈子.</p>
+                    <!-- hlinks -->
+                    <ul class="hlinks hlinks-icons color-icons-borders color-icons-bg color-icons-hovered">
+                      <li><a href="#"><i class="icon fa fa-facebook"></i></a></li>
+                      <li><a href="#"><i class="icon fa fa-twitter"></i></a></li>
+                      <li><a href="#"><i class="icon fa fa-rss"></i></a></li>
+                      <li><a href="#"><i class="icon fa fa-google-plus"></i></a></li>
+                      <li><a href="#"><i class="icon fa fa-instagram"></i></a></li>
+                      <li><a href="#"><i class="icon fa fa-youtube"></i></a></li>
+                    </ul>
+                    <!-- /hlinks -->
+                </div>
+                <!-- /Col -->
+
+                <!-- Col -->
+                <div class="col-xs-6 col-sm-3 col-md-3">
+                  <h5>会员</h5>
+                  <ul class="vlinks">
+                    <li><a href="#">帐号</a></li>
+                    <li><a href="#">历史订单</a></li>
+                    <li><a href="#">购物车</a></li>
+                  </ul>
+                </div>
+                <!-- /Col -->
+
+                <!-- Col -->
+                <div class="col-xs-6 col-sm-3 col-md-3 newsletter">
+                  <h5>订阅新品</h5>
+                  <form>
+                    <input type="text" class="form-control" placeholder="电子邮箱">
+                    <button class="btn btn-outline" type="button">订阅</button>
+                  </form>
+                </div>
+                <!-- /Col -->
+
+             </div>
+             <!-- /Row -->
+
+            </div>
+            <!-- /Links -->
+
+          </div>
+          <!-- /Row -->
+
+        </div>
+        <!-- /Container -->
+
+        <!-- Bottom -->
+        <div class="footer-bottom bcolor-bg invert-colors">
+
+          <!-- Container -->
+          <div class="container">
+
+            <span class="copy-text">© 蓓爱花坊 2014 - <?php echo date('Y')?>, All rights reserved | 京ICP备13030627号 </span>
+            <!-- hlinks -->
+            <ul class="hlinks pull-right">
+              <li><a href="#">关于我们</a></li>
+              <li><a href="#">保养说明</a></li>
+              <li><a href="/auth/login">登录</a></li>
+              <li><a href="/auth/register">注册</a></li>
+              <li><a href="#">售后服务</a></li>
+              <li><a href="#">联系我们</a></li>
+            </ul>
+            <!-- /hlinks -->
+
+          </div>
+          <!-- /Container -->
+
+        </div>
+        <!-- /Bottom -->
+
+      </footer>
+      <!-- /Footer
+      =================================================== -->
---
+++ resources/views/layouts/main.blade.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="description" content="">
+        <meta name="author" content="">
+        <title>@yield('title')</title>
+        <!-- Bootstrap core CSS
+        ================================================== -->
+        <link href="/uikit/css/bootstrap.min.css" rel="stylesheet">
+        <!-- Custom styles for this template
+        ================================================== -->
+        <link href="/uikit/css/uikit.css" rel="stylesheet">
+        <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+        <!--[if lt IE 9]>
+        <script src="/uikit/js/html5shiv.js"></script>
+        <script src="/uikit/js/respond.min.js"></script>
+        <![endif]-->
+    </head>
+    <body class="preload tile-1-bg">
+        <!-- Preloader
+        ============================================ -->
+        <div class="page-preloader">
+            <div class="vcenter"> <div class="vcenter-this"><img class="anim" src="images/loader.gif" alt="loading..." /></div></div>
+        </div>
+        <!-- /Preloader
+        ============================================ -->
+        <!-- Page Wrapper
+        1.page-wrapper shadow boxed-wrapper  ===wide
+        2.boxed-wrapper                      ===box
+        ++++++++++++++++++++++++++++++++++++++++++++ -->
+        <div class="page-wrapper shadow">
+        	@include('include.header')
+            @yield('content')
+            @include('include.footer')
+        </div>
+        <!-- /Page Wrapper
+        ++++++++++++++++++++++++++++++++++++++++++++++ -->
+        <!-- Javascript
+        ================================================== -->
+        <script src="/uikit/js/jquery-latest.min.js"></script>
+        <script src="/uikit/js/uikit.js"></script>
+        <!-- /JavaScript
+        ================================================== -->
+    </body>
+</html>
