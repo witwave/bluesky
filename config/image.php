5a417076456e0552e93cd5fe509ba9feb6ee9fb7
witwave <witwave@outlook.com>
2016-02-23 08:20:05 +0800

    image size

--- config/image.php
+++ config/image.php
@@ -6,11 +6,11 @@ return array(
     'upload_dir'  => 'assets/img',
     'upload_path' => public_path() . '/assets/img/',
     'quality'     => 85,
- 
+
     'dimensions' => array(
-        'thumb'  => array(100, 100, true,  80),
-        'medium' => array(600, 400, false, 90),
-        'large'  => array(800, 600, false, 90),
+      'thumb'  => array(100, 100, true,  80),
+      'medium' => array(600, 500, false, 90),
+      'large'  => array(709, 591, false, 90),
     ),
-    
+
 );
